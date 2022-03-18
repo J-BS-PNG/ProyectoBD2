d9d2487823fe0e0e1e94cf806e16bea25420fe75
Glori <75176699+globv28@users.noreply.github.com>
2022-03-18 00:17:59 -0600

    Proyectp_vue

---
+++ redSocial_BdII/vue_project/src/components/Register_Component.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-container fluid>
+    <b-row class="my-1" v-for="type in types" :key="type">
+      <b-col sm="3">
+        <label :for="`type-${type}`">Type <code>{{ type }}</code>:</label>
+      </b-col>
+      <b-col sm="5">
+        <b-form-input :id="`type-${type}`" :type="type"></b-form-input>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        types: [
+          'text',
+          'number',
+          'email',
+          'password',
+          'date'
+        ]
+      }
+    }
+  }
+</script>
